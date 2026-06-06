fff78d43ea0dc1aa73f111c676f2202afca39b92
burntduplo <197035802+burntduplo@users.noreply.github.com>
2026-06-06 16:33:12 +1000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Minimal Rust development environment for gidames";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            clippy
+            rust-analyzer
+            rustc
+            rustfmt
+          ];
+        };
+      }
+    );
+}
